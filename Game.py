12f6be7ab45e6c10136d682058900c34e52d27f4
Ruarc Sorensen <ruarcs@gmail.com>
2013-08-20 20:50:16 +0100

    Class summaries.

--- Game.py
+++ Game.py
@@ -1,4 +1,5 @@
 class Game:
+    'This class encapsulates one particular game.'
     def __init__(self, game_file):
         #specify the file to read the game description from
         self.game_file = game_file
--- GameRunner.py
+++ GameRunner.py
@@ -1,4 +1,5 @@
 class GameRunner:
+    'This class contains main and runs the game, asking the user for an input file.'
     def __init__(self):
         pass
     
