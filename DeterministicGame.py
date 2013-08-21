5787e6b80b0a4f40dcebbc688bf2590e2e0cd7b5
Ruarc Sorensen <ruarc.sorensen@cylon.com>
2013-08-21 10:36:03 +0100

    Added some existing code and a parallel attempt at a deterministic solver.

---
+++ DeterministicGame.py
@@ -0,0 +1,104 @@
+class DeterministicGame:
+    def __init__(self):
+        starting_player = 'X'
+        board = ['_', 'X', '_',
+                 'O', 'X', '_',
+                 'O', '_', '_']
+        vectors = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
+    def main():
+        run_game()
+
+    def run_game():
+        pass
+
+    def make_move(self):
+        made_move = False
+        won = False
+
+        made_move = win()
+        if ~made_move:
+            made_move = block()
+        else:
+            won = True
+
+        if ~made_move:
+            made_move = fork()       
+ 
+        if ~made_move:
+            made_move = block_fork() 
+
+        if ~made_move:
+            made_move = center() 
+
+        if ~made_move:
+            made_move = opposite_corner() 
+
+        if ~made_move:
+            made_move = empty_corner() 
+
+        if ~made_move:
+            made_move = empty_side()
+
+        print_board_state()
+
+    def win(self):
+        action = 'win'
+        for vector in vectors:
+            if analyse_vector(action):
+                return True
+        return False
+
+    def block(self):
+        action = 'block'
+        for vector in vectors:
+            if analyse_vector(action):
+                return True
+        return False
+
+    def fork(self):
+        return False
+
+    def block_fork(self):
+        return False
+
+    def center(self):
+        return False
+
+    def opposite_corner(self):
+        return False
+
+    def empty_corner(self):
+        return False
+
+    def empty_side(self):
+        return False
+
+    ###################################
+
+    def analyse_vector(self,action):
+        pass
+
+    def print_board_state(self):
+        print(board[0]),
+        print('|'),
+        print(board[1]),
+        print('|'),
+        print(board[2]),
+        print('----')
+        print(board[3]),
+        print('|'),
+        print(board[4]),
+        print('|'),
+        print(board[5]),
+        print('----')
+        print(board[6]),
+        print('|'),
+        print(board[7]),
+        print('|'),
+        print(board[8])
+
+    ###################################
+
+    if __name__ == '__main__':
+        main()
--- Game.py
+++ Game.py
@@ -4,3 +4,82 @@ class Game:
         #specify the file to read the game description from
         self.game_file = game_file
         
+
+    #imported code
+    def __init__(self):
+        starting_player = 'X'
+        board = ['_', 'X', '_',
+                 'O', 'X', '_',
+                 'O', '_', '_']
+        vectors = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
+    def run(self):
+        minimax_recursive(0, 0, 0, self.starting_player, self.board)
+
+    def minimax_recursive(self, depth, a, b, player, board):
+        if depth > MAX_DEPTH:
+            return heuristic_score(board, player)
+        else:           
+            alpha = a;
+            beta = b;
+            if player != starting_player:   #min layer
+                for #all child nodes
+                alpha = minimax_recursive()
+            else:
+                beta = minimax_recursive()
+
+    def heuristic_score(board, player):
+        alpha = 0
+        beta = 0
+        for vector in vectors:  #check each vector
+            alpha += analyse_vector(board, vector, player)
+            beta -= analyse_vector(board, vector, other_player(player))
+        return alpha + beta
+
+    def analyse_vector(board, vector, player):
+        entries = count_entries_in_vector(board, vector, player)
+        if entries == 3:
+            return 1000
+        else if entries == 2:
+            return 100
+        else if entries == 1:
+            return 10
+
+    def count_entries_in_vector(board, vector, player):
+        count = 0
+        for index in vector:
+            if board[index] == player:
+                count++
+        return count
+
+    def other_player(player):
+        if player == 'X':
+            return 'O'
+        else:
+            return 'X'
+            
+    ################################################
+    ############### Utility Methods ################
+    ################################################
+        
+    # print the board state each time a move is made
+    def print_board_state(self):
+        print(board[0]),
+        print('|'),
+        print(board[1]),
+        print('|'),
+        print(board[2]),
+        print('----')
+        print(board[3]),
+        print('|'),
+        print(board[4]),
+        print('|'),
+        print(board[5]),
+        print('----')
+        print(board[6]),
+        print('|'),
+        print(board[7]),
+        print('|'),
+        print(board[8])
+                    
+        
